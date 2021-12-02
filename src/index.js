825f36771a17047de583448236c06c019e3f6c96
pushp <psdamncool7@gmail.com>
2021-12-02 11:48:54 +0530

    auth check

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
   import { getDatabase, ref, set, get, child, update, remove } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
+  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js";
+
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +22,7 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const db = getDatabase();
-
+  const auth = getAuth();
   var nam = document.getElementById("name");
   var designation = document.getElementById("designation");
   var floor = document.getElementById("floor");
@@ -96,4 +98,3 @@
     })
   }
 
-  
